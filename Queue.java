a5931f0e1f2b368ed2c3d32f3f2de1b92e9471c8
Christian Marcy <christian.marcy@gmail.com>
2017-12-02 18:37:53 -0500

    Updated Queue file

--- Queue.java
+++ Queue.java
@@ -1,107 +1,133 @@
 /*
- * Purpose: Data Structure and Algorithms Lab 6 Problem 1
+ * Purpose: Data Structure and Algorithms Lab 6 Problem 1/PreLab
  * Status: Complete and thoroughly tested
- * Last update: 10/11/17
- * Submitted: 10/12/17
+ * Last update: 10/11/2017
+ * Submitted:  10/11/2017
  * Comment: test suite and sample run attached
- * @author: Kunal Bodani
+ * @author: Christian Marcy
  * @version: 2017.10.11
  */
-
-public class Queue<T> implements QueueInterface<T>
-{
-	protected T[] array;
+public class Queue<T> implements QueueInterface<T> {
+	
 	protected int front;
 	protected int back;
+	protected T[] items;
 	protected int numItems;
-
-	@SuppressWarnings("unchecked")
+	
+	/*
+	 * Initializes all fields of a Queue
+	 */
 	public Queue()
 	{
-		array = (T[]) new Object[3];
 		front = 0;
 		back = 0;
+		items = (T[])new Object[3];
 		numItems = 0;
 	}
-
+	
+	/*
+	 * Returns true if the items array is empty
+	 */
 	public boolean isEmpty()
 	{
 		return numItems == 0;
 	}
 	
-	public int size()
+	/*
+	 * Adds an item to the endf of the queue in a circular fashion
+	 * @param newItem The new item to be added to the queue
+	 */
+	public void enqueue(T newItem)
 	{
-		return numItems;
-	}
-
-	public void enqueue(T newItem) throws QueueException
-	{
-		if (numItems == array.length)
+		// If the queue is full, resize the array
+		if(numItems == items.length) {
 			resize();
-
-		array[back] = newItem;
-		back = (back + 1) % array.length;
-
+		}
+		
+		// Insert item in to items[back]
+		items[back] = newItem;
+		
+		// increment back in a circular fashion
+		back = (back+1)%items.length;
 		numItems++;
 	}
-
+	
+	/*
+	 * Deletes an item from the beginning of the queue in a circular fashion
+	 * @return Returns the item that was deleted from the beginning of the queue
+	 * @throws Throws an exception when the queue is empty
+	 */
 	public T dequeue() throws QueueException
 	{
-		if (numItems > 0)
-		{
-			T t = array[front];
-			array[front] = null;
-
-			front = (front + 1) % array.length;
+		if(numItems != 0) {
+			// Save the item at the front to dequeue
+			T thing = items[front];
+						
+			// drop null to prevent memory leak
+			items[front] = null;
+						
+			//increment front in a circular fashion
+			front = (front+1)%items.length;
 			numItems--;
-
-			return t;
+			return thing;
+		} else {
+			throw new QueueException("List is empty.");
 		}
-
-		else throw new QueueException("QueueException on dequeue");
 	}
-
-	@SuppressWarnings("unchecked")
+	
+	/*
+	 * Dequeues all items in the queue and resizes array back to original size
+	 */
 	public void dequeueAll()
 	{
-		if (numItems > 0)
-		{
-			array = (T[]) new Object[3];
-			front = 0;
-			back = 0;
-			numItems = 0;
-		}
-
-		else throw new QueueException("QueueException on dequeueAll");
+		front = 0;
+		back = 0;
+		items = (T[])new Object[3];
+		numItems = 0;
 	}
-
+	
+	/*
+	 * Returns the item at the front currently
+	 * @return Returns the item at the beginning of the queue, does not remove it
+	 * @throws Throws exception when the queue is empty
+	 */
 	public T peek() throws QueueException
 	{
-		if (numItems > 0)
-			return array[front];
-
-		else throw new QueueException("QueueException on peek");
+		if(numItems != 0) {
+			return items[front];
+		} else {
+			throw new QueueException("Empty queue.");
+		}
 	}
-
-	public String toString() 
+	
+	/*
+	 * Returns a string representation of the queue
+	 * @return Formatted information of the items array
+	 */
+	public String toString()
 	{
-		String s = "";
-
-		for (int i = 0; i < numItems; i++)
-			s += array[(front + i) % array.length] + " ";
-
-		return s;
+		String result = "Queue of size " + numItems + " contains: ";
+		
+		for(int i = 0; i < numItems; i++) {
+			result += items[(front+i)%items.length] + " ";
+			//f = (f+1)%items.length;
+		}
+		
+		return result;
 	}
-
-	@SuppressWarnings("unchecked")
+	
+	/*
+	 * Resizes the items array to become bigger
+	 */
 	protected void resize()
 	{
-		T[] temp = (T[]) new Object[array.length * 2];
-
-		for (int i = 0; i < numItems; i++)
-			temp[i] = array[(front + i) % array.length];
-
-		array = temp;
+		T[] large = (T[])new Object[items.length*2 + 1];
+		
+		for(int i = 0; i < numItems; i++) {
+			large[i] = items[(front+i)%items.length];
+		}
+		
+		items = large;
 		front = 0;
 		back = numItems;
 	}
