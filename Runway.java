238a58928d83a8466a91ff4b2179a07d067a6a97
Christian Marcy <christian.marcy@gmail.com>
2017-12-02 20:05:18 -0500

    Added necessary methods

--- Runway.java
+++ Runway.java
@@ -3,25 +3,68 @@ public class Runway
 {
 	private String name;
 	private Queue<Flight> flightQueue;
+	private MyListReferenceBased<Flight> waitingList;
 	
+	/*
+	 * Creates a runway with a specific name
+	 * @param name The name of the run-way
+	 */
 	public Runway(String name)
 	{
 		this.name = name;
 		flightQueue = new Queue<Flight>(); 
+		waitingList = new MyListReferenceBased<Flight>();
 	}
 	
-	public void add(Flight flight)
-	{
-		flightQueue.enqueue(flight);
-	}
+//  <-------------------------------------------->  //
 	
+	/*
+	 * Returns the queue of planes waiting to take off
+	 * @return Returns the queue of the 
+	 */
 	public Queue<Flight> getFlightQueue()
 	{
 		return flightQueue;
 	}
 	
+	/*
+	 * Returns the list of planes waiting to re-enter the run-way
+	 */
+	public MyListReferenceBased<Flight> getWaitingList()
+	{
+		return waitingList;
+	}
+	
+	/*
+	 * Returns the name of the runway
+	 */
 	public String getName()
 	{
 		return name;
 	}
-}
+	
+	
+//  <-------------------------------------------->  //
+	
+	
+	/*
+	 * Adds a specific flight to the run way. The plane
+	 * is eligible for take-off
+	 * @param flight The flight to be added 
+	 */
+	public void addToRunway(Flight flight)
+	{
+		flightQueue.enqueue(flight);
+	}
+	
+	public void addToWaiting(Flight flight)
+	{
+		waitingList.add
+	}
+	
+	
+	
+	
+	
+	
+} // end of class
