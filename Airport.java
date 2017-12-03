a2ce491ba30700eee16c50bb79ab58fddd795701
Christian Marcy <christian.marcy@gmail.com>
2017-12-03 12:27:17 -0500

    Added setters and getters

--- Airport.java
+++ Airport.java
@@ -1,4 +1,42 @@
 
 public class Airport {
 
+	private Queue<Runway> runways;
+	private int planeCount;
+	
+	/*
+	 * Creates attributes of an airport
+	 * @param runWayCount The number of run-ways for the airport
+	 */
+	public Airport(int runwayCount) {
+		runways = new Queue<Runway>();
+		
+		planeCount = 0;
+	}
+	
+//  <-------------------------------------------->  //
+	
+	/*
+	 * Returns all run-ways in the airport
+	 * @return The runways in the airport
+	 */
+	public Queue<Runway> getRunways()
+	{
+		return runways;
+	}
+	
+	/*
+	 * Returns the count of how many planes left the airport
+	 * @return The count of planes that left the airport
+	 */
+	public int getPlaneCount()
+	{
+		return planeCount;
+	}
+	
+//  <-------------------------------------------->  //
+
+	
+	
+	
 }
